cee23c78342b8bdc2ec4d83343a50a3e0b31a3ba
rzvxa <rzvxa@protonmail.com>
2024-07-07 21:52:31 +0330

    feat(allocator): introduce `FromIn` and `IntoIn` traits.

---
+++ crates/oxc_allocator/src/convert.rs
@@ -0,0 +1,53 @@
+#![allow(clippy::inline_always)]
+
+use crate::{Allocator, Box};
+
+pub trait FromIn<'a, T> {
+    fn from_in(value: T, alloc: &'a Allocator) -> Self;
+}
+
+pub trait IntoIn<'a, T> {
+    fn into_in(self, alloc: &'a Allocator) -> T;
+}
+
+/// `FromIn` is reflective
+impl<'a, T> FromIn<'a, T> for T {
+    #[inline(always)]
+    fn from_in(t: T, _: &'a Allocator) -> T {
+        t
+    }
+}
+
+/// `FromIn` implicitly implements `IntoIn`.
+impl<'a, T, U> IntoIn<'a, U> for T
+where
+    U: FromIn<'a, T>,
+{
+    #[inline]
+    fn into_in(self, alloc: &'a Allocator) -> U {
+        U::from_in(self, alloc)
+    }
+}
+
+// ---------------- Primitive allocations ----------------
+
+impl<'a> FromIn<'a, String> for crate::String<'a> {
+    #[inline(always)]
+    fn from_in(value: String, alloc: &'a Allocator) -> Self {
+        crate::String::from_str_in(value.as_str(), alloc)
+    }
+}
+
+impl<'a, T> FromIn<'a, T> for Box<'a, T> {
+    #[inline(always)]
+    fn from_in(value: T, alloc: &'a Allocator) -> Self {
+        Box::new_in(value, alloc)
+    }
+}
+
+impl<'a, T> FromIn<'a, Option<T>> for Option<Box<'a, T>> {
+    #[inline(always)]
+    fn from_in(value: Option<T>, alloc: &'a Allocator) -> Self {
+        value.map(|it| Box::new_in(it, alloc))
+    }
+}
